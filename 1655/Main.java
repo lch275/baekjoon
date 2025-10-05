57c132a5637be2a09ca6c157fde03945f8a5f20f
lch275 <chanho.lee0817@gmail.com>
2025-10-05 10:53:54 +0900

    feat: 우선순위 큐 문제 해결
    - 정렬로 접근하여, binary search tree를 시도하였으나, prioirty heap을
      2개로 나누어 중간을 찾는 것이 더 빠르게 이루어짐.
    
    resolve: https://www.acmicpc.net/problem/1655

---
+++ 1655/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+
+            if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+                maxHeap.offer(num);
+            } else {
+                minHeap.offer(num);
+            }
+
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.offer(maxHeap.poll());
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.offer(minHeap.poll());
+            }
+
+            System.out.println(maxHeap.peek());
+        }
+    }
+}
